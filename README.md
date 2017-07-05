e31dd3daf3892dc9d4a6a6fd7180fcc60eb49ad8
Martin Meredith <martin.meredith@stickee.co.uk>
2017-07-05 16:48:44 +0100

    Add ESLint Task

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-GrumPHP Tasks
+# GrumPHP Tasks
 
+Requires NPM packages too!
+
+Example package.json
+```json
+{
+  "dependencies": {
+    "eslint": "^4.1.1",
+    "eslint-config-standard": "^10.2.1",
+    "eslint-plugin-promise": "^3.5.0",
+    "eslint-plugin-standard": "^3.0.1"
+  }
+}
+```
+
+
+
+Example `grumphp.yml`
+
+```yaml
+parameters:
+  git_dir: .
+  bin_dir: vendor/bin
+  tasks:
+    eslint: ~
+services:
+  task.eslint:
+    class: Stickee\GrumPHP\ESLint
+    arguments:
+      - '@config'
+      - '@process_builder'
+      - '@formatter.raw_process'
+    tags:
+      - {name: grumphp.task, config: eslint}
+
+```
---
+++ src/ESLint.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * @copyright (c) 2006-2017 Stickee Technology Limited
+ */
+
+namespace Stickee\GrumPHP;
+
+use GrumPHP\Collection\FilesCollection;
+use GrumPHP\Collection\ProcessArgumentsCollection;
+use GrumPHP\Runner\TaskResult;
+use GrumPHP\Runner\TaskResultInterface;
+use GrumPHP\Task\AbstractExternalTask;
+use GrumPHP\Task\Context\ContextInterface;
+use GrumPHP\Task\Context\GitPreCommitContext;
+use GrumPHP\Task\Context\RunContext;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class ESLint
+ *
+ * @package Stickee\GrumPHP
+ */
+final class ESLint extends AbstractExternalTask
+{
+    /**
+     * getName
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'eslint';
+    }
+
+    /**
+     * getconfiguration
+     *
+     * @return array
+     */
+    public function getConfiguration()
+    {
+        $configured = $this->grumPHP->getTaskConfiguration($this->getName());
+
+        return $this->getConfigurableOptions()->resolve($configured);
+    }
+
+    /**
+     * getConfigurableOptions
+     *
+     * @return OptionsResolver
+     */
+    public function getConfigurableOptions()
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults(
+            [
+                'config' => null,
+                'debug' => false,
+            ]
+        );
+
+        $resolver->addAllowedTypes('config', ['null', 'string']);
+        $resolver->addAllowedTypes('debug', ['bool']);
+
+        return $resolver;
+    }
+
+    /**
+     * This methods specifies if a task can run in a specific context.
+     *
+     * @param ContextInterface $context
+     *
+     * @return bool
+     */
+    public function canRunInContext(ContextInterface $context)
+    {
+        return ($context instanceof GitPreCommitContext || $context instanceof RunContext);
+    }
+
+    /**
+     * @param ContextInterface $context
+     *
+     * @return TaskResultInterface
+     */
+    public function run(ContextInterface $context)
+    {
+        $files = $context->getFiles()->name('*.js');
+        if (0 === count($files)) {
+            return TaskResult::createSkipped($this, $context);
+        }
+
+        $config = $this->getConfiguration();
+
+        $arguments = $this->processBuilder->createArgumentsForCommand('eslint');
+        $arguments->add('--format=table');
+        $arguments->addOptionalArgument('--config %s', $config['config']);
+        $arguments->addOptionalArgument('--debug', $config['debug']);
+
+        if ($context instanceof RunContext && $config['config_file'] !== null) {
+            return $this->runOnAllFiles($context, $arguments);
+        }
+
+        return $this->runOnChangedFiles($context, $arguments, $files);
+    }
+
+    /**
+     * @param ContextInterface $context
+     * @param ProcessArgumentsCollection $arguments
+     * @param FilesCollection $files
+     *
+     * @return TaskResult
+     */
+    private function runOnChangedFiles(
+        ContextInterface $context,
+        ProcessArgumentsCollection $arguments,
+        FilesCollection $files
+    ) {
+        $hasErrors = false;
+        $messages = [];
+
+        foreach ($files as $file) {
+            $fileArguments = new ProcessArgumentsCollection($arguments->getValues());
+            $fileArguments->add($file);
+            $process = $this->processBuilder->buildProcess($fileArguments);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                $hasErrors = true;
+                $messages[] = $this->formatter->format($process);
+            }
+        }
+
+        if ($hasErrors) {
+            $errorMessage = sprintf("You have ESLint Errors:\n\n%s", implode("\n", $messages));
+
+            return TaskResult::createFailed($this, $context, $errorMessage);
+        }
+
+        return TaskResult::createPassed($this, $context);
+    }
+
+    /**
+     * @param ContextInterface $context
+     * @param ProcessArgumentsCollection $arguments
+     *
+     * @return TaskResult
+     */
+    private function runOnAllFiles(ContextInterface $context, ProcessArgumentsCollection $arguments)
+    {
+        $process = $this->processBuilder->buildProcess($arguments);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $errorMessage = $this->formatter->format($process);
+
+            return TaskResult::createFailed($this, $context, $errorMessage);
+        }
+
+        return TaskResult::createPassed($this, $context);
+    }
+}
